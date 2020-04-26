2c0a085853681c61cf547e91811e799bd236a84e
JeesooHaa <haajeesoo@gmail.com>
2020-04-26 17:32:06 +0900

    feat: Add background image
     - add background image
     - change favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <p>JeesooHaa</p>
-      <p>Frontend developer</p>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
